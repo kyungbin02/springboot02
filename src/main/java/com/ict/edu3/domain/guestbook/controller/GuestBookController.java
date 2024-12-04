524ea1e5ed1d9c4bae41c0ec9bc585bfddc072fa
kyungbin <thrudqls13@naver.com>
2024-12-04 11:45:08 +0900

    게스트 북 수정 완료

--- src/main/java/com/ict/edu3/domain/guestbook/controller/GuestBookController.java
+++ src/main/java/com/ict/edu3/domain/guestbook/controller/GuestBookController.java
@@ -3,6 +3,7 @@ package com.ict.edu3.domain.guestbook.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +14,8 @@ import com.ict.edu3.domain.guestbook.vo.GuestBookVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Slf4j
 @RestController
@@ -38,7 +40,7 @@ public class GuestBookController {
     }
 
     @GetMapping("/detail/{gb_idx}")
-    public DataVO getGuestBookDetail(@PathVariable String gb_idx) {
+    public DataVO getGuestBookDetail(@PathVariable("gb_idx") String gb_idx) {
         DataVO dataVO = new DataVO();
         try {
             // log.info("gb_idx : " + gb_idx);
@@ -59,10 +61,15 @@ public class GuestBookController {
     }
 
     @GetMapping("/delete/{gb_idx}")
-    public DataVO getGuestBookDelete(@PathVariable String gb_idx) {
+    public DataVO getGuestBookDelete(@PathVariable("gb_idx") String gb_idx, Authentication authentication) {
         DataVO dataVO = new DataVO();
         try {
-
+            // 로그인 여부 확인
+            if (authentication == null) {
+                dataVO.setSuccess(false);
+                dataVO.setMessage("로그인이 필요합니다.");
+                return dataVO;
+            }
             int result = guestBookService.getGuestBookDelete(gb_idx);
             if (result == 0) {
                 dataVO.setSuccess(false);
@@ -75,6 +82,32 @@ public class GuestBookController {
         } catch (Exception e) {
             dataVO.setSuccess(false);
             dataVO.setMessage("게스트북 삭제 오류 발생");
+        }
+        return dataVO;
+    }
+    @PutMapping("/update/{gb_idx}")
+    public DataVO getGuestBookUpdate(@PathVariable("gb_idx") String gb_idx, @RequestBody GuestBookVO gvo, Authentication authentication) {
+        DataVO dataVO = new DataVO();
+        try {
+            // 로그인 여부 확인
+            if (authentication == null) {
+                dataVO.setSuccess(false);
+                dataVO.setMessage("로그인이 필요합니다.");
+                return dataVO;
+            }
+            int result = guestBookService.getGuestBookUpdate(gvo);
+            
+            if (result == 0) {
+                dataVO.setSuccess(false);
+                dataVO.setMessage("게스트북 수정 실패");
+                return dataVO;
+            }
+            dataVO.setSuccess(true);
+            dataVO.setMessage("게스트북 수정 성공");
+
+        } catch (Exception e) {
+            dataVO.setSuccess(false);
+            dataVO.setMessage("게스트북 수정 오류 발생");
         }
         return dataVO;
     }
